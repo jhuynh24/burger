7b4891369724e73a8c46ff01c288605d8865b7cb
jhuynh24 <jhuynh1735@gmail.com>
2020-12-15 13:41:54 -0600

    adding database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', TRUE);
+
