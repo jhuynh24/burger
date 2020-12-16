046f96837fa2ad7e1e288b766aee69280bc5c7dc
jhuynh24 <jhuynh1735@gmail.com>
2020-12-15 22:15:49 -0600

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { 
+  connection = mysql.createConnection({
     host: "localhost",
 
     port: 3306,
@@ -9,7 +15,7 @@ var connection = mysql.createConnection({
     password: "Fghfgh77",
     database: "burgers_db"
   });
-
+}
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
