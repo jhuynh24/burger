235292c61e50c2b111df32a5ec23b5f4dec87f2d
jhuynh24 <jhuynh1735@gmail.com>
2020-12-15 19:01:42 -0600

    fixing orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,30 @@
 var connection = require('./connection');
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
+
 var orm = {
     selectAll: function (table_name, cb) {
-        var queryString = 'SELECT * FROM' + table_name + ';';
+        var queryString = 'SELECT * FROM ' + table_name + ';';
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
@@ -19,15 +41,22 @@ var orm = {
             cb(result);
         });
     },
-    updateOne: function (table_name, object, condition, cb) {
-        var queryString = 'UPDATE ' + table_name + ' SET ? WHERE ?;';
-        connection.query(queryString, [object, condition], function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
+    updateOne: function(table, object, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(object);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
         });
-    }
+      }
 }
 
 module.exports = orm;
