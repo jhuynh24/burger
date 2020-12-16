c054034abfc252f0491839d570d3faf4547f76f2
jhuynh24 <jhuynh1735@gmail.com>
2020-12-15 23:24:52 -0600

    adding db to the seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+USE burgers_db
 INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', TRUE);
